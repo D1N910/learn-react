65139fc022b31fe0a1841575dabfc4e25740850f
D1n910 <wyd1n910@gmail.com>
2018-10-24 17:33:10 +0800

    复杂的逻辑

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import ReactDOM from 'react-dom';
 
 import './index.less';
 
-class Hello extends React.Component{
-  render() { 
-    const name = 'react'
-    return <div>Wellcome! {name||'user'}!{
-      !name && <p>You need log in</p>
-    }</div>
+class UserInfo extends React.Component{
+  render() {
+    let dom = <div></div>;
+    if (this.props.type == 'admin') {
+      dom = <p>You are admin.</p>
+    } else if (this.props.type == 'vip') {
+      dom = <p>You are vip.</p>
+    } else {
+      dom = <p>Hello React.</p>
+    }
+
+    return dom;
   }
 }
-ReactDOM.render(<Hello name="D1n910" />, document.querySelector('#app'));
+ReactDOM.render(<UserInfo type="admin" />, document.querySelector('#app'));
