89d6853cbc4e7cb30037f429dc8bf8b7d6f6bed5
D1n910 <wyd1n910@gmail.com>
2018-10-24 15:39:03 +0800

    解决了bug

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   externals: {
-    "rect":'React',
-    'react-dom': 'ReactDom'
+    "react": 'React',
+    "react-dom": 'ReactDOM'
   }
 }
