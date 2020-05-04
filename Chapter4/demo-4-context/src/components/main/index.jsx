d2ac93eeda7efaab134a102b548b9f3ce9341677
D1N910 <wyD1n910@gmail.com>
2020-05-05 04:15:25 +0800

    econtext

---
+++ Chapter4/demo-4-context/src/components/main/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const ThemeContext = React.createContext('light')
+
+export default class Main extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			theme: 'light'
+		}
+	}
+
+	changeTheme(e) {
+		this.setState({
+			theme: e.currentTarget.value
+		})
+	}
+
+	render() {
+		return (
+			<ThemeContext.Provider value={this.state.theme}>
+				<Toolbar />
+				<div>
+					<label>darl:</label>
+					<input type="radio" value="dark" onChange={this.changeTheme.bind(this)} checked={this.state.theme === 'dark'}/>
+					<label>light:</label>
+					<input type="radio" value="light" onChange={this.changeTheme.bind(this)} checked={this.state.theme === 'light'}/>
+				</div>
+			</ThemeContext.Provider>
+		)
+  }
+}
+class ThemedButton extends React.Component {
+	static contextType = ThemeContext;
+
+  render() {
+    return <button className={this.context + "-button"}>ContextButton</button>
+  }
+}
+
+function Toolbar(props) {
+	return (
+			<div>
+					<ThemedButton />
+			</div>
+	);
+}
