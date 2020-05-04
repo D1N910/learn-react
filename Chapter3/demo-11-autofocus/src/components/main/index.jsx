6d042dc4c16f0bb51d3e7eda773131c2bf4991d5
D1N910 <wyD1n910@gmail.com>
2020-05-04 17:25:44 +0800

    添加了函数式组件

--- Chapter3/demo-11-autofocus/src/components/main/index.jsx
+++ Chapter3/demo-11-autofocus/src/components/main/index.jsx
@@ -1,21 +1,75 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+// 函数式组件 访问 context
+const Text = (props, context) => <p style={context}>{props.children}</p>
+Text.contextTypes = {
+  color: PropTypes.string
+};
+
+export default class Main extends React.Component {
+	/**
+	 * 想要传递 context，需要定义childContextTypes
+	 * 而且因为 babel 转换问题，这种静态的写法是 ES7 的内容，所以要在配置 .babelrc 配置 stage-1
+	*/
+
+  static childContextTypes = {
+    color: PropTypes.string
+	}
+
+	// 传递给子组件的 context
+  getChildContext() {
+    return {
+      color: 'red'
+    };
+	}
+
+  render() {
+    return <Text>Hello World</Text>;
+  }
+}
+
+// 函数式组件
+// const Text = (props) => <p>{props.children}</p>
+
+// export default class Main extends React.Component {
+// 	render() {
+// 		return <Text>你好，函数式组件</Text>
+// 	}
+// }
+
+// 函数式组件 class
+// class Text extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 	}
+// 	render() {
+// 		return <p>{this.props.children}</p>
+// 	}
+// }
+
+// export default class Main extends React.Component {
+// 	render() {
+// 		return <Text>你好，函数式组件</Text>
+// 	}
+// }
 
 // 方法2、通过回调 Refs 获取 DOM 节点
-export default class Main extends React.Component {
-	constructor(props) {
-		super(props);
-		this.myRef = null
-	}
+// export default class Main extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.myRef = null
+// 	}
 
-	componentDidMount() {
-		this.myRef.focus()
-		this.myRef.value = '成功'
-	}
+// 	componentDidMount() {
+// 		this.myRef.focus()
+// 		this.myRef.value = '成功'
+// 	}
 
-	render() {
-		return <input ref={ref => {this.myRef = ref}} />
-	}
-}
+// 	render() {
+// 		return <input ref={ref => {this.myRef = ref}} />
+// 	}
+// }
 
 // 方法1、使用 React.createRef & ref.current 来定义并获取 Refs
 // export default class Main extends React.Component {
